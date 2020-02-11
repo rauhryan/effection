2fed466a8f794c897b3f6733edd91998135dbfad
Charles Lowell <cowboyd@frontside.io>
2020-02-11 14:18:18 -0600

    Add a type parameter for Operation.
    This is the type that is return by an operation

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,38 @@
 // TypeScript Version: 3.7
 declare module "effection" {
-  export type Operation = OperationFn | Sequence | Promise<any> | Controller | undefined;
+  export type Operation<T> = OperationFn<T> | Sequence<T> | Promise<T> | Controller<T> | undefined;
 
-  type OperationFn = () => Operation;
+  type OperationFn<T> = () => Operation<T>;
 
-  type Controller = (controls: Controls) => void | (() => void);
+  type Controller<T> = (controls: Controls<T>) => void | (() => void);
 
-  interface Sequence extends Generator<Operation, any, any> {}
+  interface Sequence<T> extends Generator<Operation<unknown>, T, any> {}
 
-  export interface Context extends PromiseLike<any> {
+  export interface Context<T> extends PromiseLike<T> {
     id: number;
-    parent?: Context;
-    result?: any;
+    parent?: Context<unknown>;
+    result?: T;
     halt(reason?: any): void;
     catch<R>(fn: (error: Error) => R): Promise<R>;
     finally(fn: () => void): Promise<any>;
   }
 
-  export interface Controls {
+  export interface Controls<T> {
     id: number;
-    resume(result?: any): void;
+    resume(result: T): void;
     fail(error: Error): void;
-    ensure(hook: (context?: Context) => void): () => void;
-    spawn(operation: Operation): Context;
-    context: Context;
+    ensure(hook: (context?: Context<T>) => void): () => void;
+    spawn<T>(operation: Operation<T>): Context<T>;
+    context: Context<T>;
   }
 
-  export function main(operation: Operation): Context;
+  export function main<T>(operation: Operation<T>): Context<T>;
 
-  export function fork(operation: Operation): Operation;
+  export function fork<T>(operation: Operation<T>): Context<T>;
 
-  export function join(context: Context): Operation;
+  export function join<T>(context: Context<T>): Operation<T>;
 
-  export function monitor(operation: Operation): Operation;
+  export function monitor<T>(operation: Operation<T>): Context<T>;
 
-  export function timeout(durationMillis: number): Operation;
+  export function timeout(durationMillis: number): Operation<void>;
 }
